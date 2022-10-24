a63932845413574223b0a219adff50e6e3b02a2b
AnyBananaGAME <ainaraspirmas@gmail.com>
2022-10-24 16:15:03 +0300

    Readline eeeeeeeeeeeeee

--- index.js
+++ index.js
@@ -1,21 +1,43 @@
 const bedrock = require('bedrock-protocol')
 const chalk = require("chalk")
+const readline = require("readline")
+
 const client = bedrock.createClient({
   host: 'aetherpe.net',   
   port: 19132,       
   offline: false
 })
+const rl = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout
+})
 
+
+function listenToUserInput() {
+  return rl.question("", (response) => {
+
+    client.queue('text', {
+      type: 'chat', needs_translation: false, source_name: client.username, xuid: '', platform_chat_id: '',
+      message: `${response}`
+    })
+
+    listenToUserInput()
+  })
+}
 
 client.on('text', (packet) => { 
+  
   const ORANGE = chalk.hex('#FFA500'); 
   const GREEN = chalk.hex('#00FF00'); 
   const YELLOW = chalk.hex('#FFFF00'); 
   const BLUE = chalk.hex('#0000FF'); 
-  const args = packet.message.slice().trim().split(/ +/);
+  const LGRAY = chalk.hex('#D3D3D3')
 
 
 
+  if(packet.message == "death.attack.player"){
+    console.log(chalk.red(packet.parameters[0]) + YELLOW(` was slain by `) + GREEN(packet.parameters[1]))
+  }
 
 
   const STR = packet.message.toString()
@@ -39,10 +61,12 @@ client.on('text', (packet) => {
   let r17 = r16.replaceAll('§c', '')
   let r18 = r17.replaceAll('§b', '')
   let r19 = r18.replaceAll('§o', '')
+  
 
-  console.log(r19)
-
+  console.log(LGRAY(r19))
+  
 
 
 
 })
+listenToUserInput()
